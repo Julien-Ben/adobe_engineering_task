216419108d148e649131fc21814e4a1e5ac9520c
Julien-Ben <33035980+Julien-Ben@users.noreply.github.com>
2022-03-09 13:12:08 +0100

    Checking for valid range

--- src/main/java/com/example/aem_engineering_test/RomanNumeralController.java
+++ src/main/java/com/example/aem_engineering_test/RomanNumeralController.java
@@ -1,5 +1,7 @@
 package com.example.aem_engineering_test;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,11 +9,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class RomanNumeralController {
-    private static final String template = "Hello, %s!";
 
-    @GetMapping("/hello-world")
+    @GetMapping("/romannumeral")
     @ResponseBody
-    public RomanNumeralResponseBody romanNumeralEndpoint(@RequestParam(name="inputVal", required=false, defaultValue="1") String inputVal) {
-        return new RomanNumeralResponseBody(inputVal, "output");
+    public ResponseEntity<?> romanNumeralEndpoint(@RequestParam(name="query", required=false, defaultValue="1") int inputVal) {
+        if (inputVal<1 || inputVal>255) {
+            return new ResponseEntity<>("Please provide an integer in the range 1-255", HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(new RomanNumeralResponseBody(inputVal+"", "output"), HttpStatus.OK);
     }
 }
