8f71031ef8104823975700d7671017e377866b6d
Julien-Ben <33035980+Julien-Ben@users.noreply.github.com>
2022-03-09 14:20:14 +0100

    Unit tests

---
+++ src/test/java/com/example/aem_engineering_test/RomanNumeralConverterSpec.java
@@ -0,0 +1,26 @@
+package com.example.aem_engineering_test;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RomanNumeralConverterSpec {
+
+    @Test
+    @DisplayName("RomanNumeralConvert should throw an exception when input value is out of range")
+    void romanNumeralConvertThrowsException() {
+        assertThrows(IllegalArgumentException.class, () -> RomanNumeralConverter.romanNumeralConvert(0));
+        assertThrows(IllegalArgumentException.class, () -> RomanNumeralConverter.romanNumeralConvert(256));
+    }
+
+    @Test
+    @DisplayName("RomanNumeralConvert should work for a list of known values")
+    void romanNumeralConvertIsCorrect() {
+        int[] inputList = {1, 3, 5, 18, 24, 78, 82, 97, 165, 255};
+        String[] expectedOutputList = {"I", "III", "V", "XVIII", "XXIV", "LXXVIII", "LXXXII", "XCVII", "CLXV", "CCLV"};
+        for (int i = 0; i < inputList.length; i++) {
+            assertEquals(expectedOutputList[i], RomanNumeralConverter.romanNumeralConvert(inputList[i]));
+        }
+    }
+}
